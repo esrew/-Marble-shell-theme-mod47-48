befc0ee296c292f59879779f03117eb9fb19cf69
esrew <129062548+esrew@users.noreply.github.com>
2025-02-19 18:40:36 +0200

    Add files via upload

---
+++ scripts/config.py
@@ -0,0 +1,18 @@
+# folder definitions
+temp_folder = ".temp"
+gnome_folder = "gnome-shell"
+temp_gnome_folder = f"{temp_folder}/{gnome_folder}"
+tweaks_folder = "tweaks"
+themes_folder = "~/.themes"
+raw_theme_folder = "theme"
+scripts_folder = "scripts"
+
+# GDM definitions
+global_gnome_shell_theme = "/usr/share/gnome-shell"
+gnome_shell_gresource = "gnome-shell-theme.gresource"
+extracted_gdm_folder = "theme"
+
+# files definitions
+gnome_shell_css = f"{temp_gnome_folder}/gnome-shell.css"
+tweak_file = f"./{tweaks_folder}/*/tweak.py"
+colors_json = "colors.json"
